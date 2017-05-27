c9143c75d735f153526a8b57e67fc534949e18ca
Lysandro Carioca <lysandroc@gmail.com>
2017-05-27 17:18:53 -0300

    static files to heroku

--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 
 app.use(express.static('./public')); 
+app.use(bodyParser.json());
 
-app.get('/', function(request, response) {
-  response.render('./public/index.html');
-});
+// app.get('/', function(request, response) {
+//   response.render('./public/index.html');
+// });
 
-app.listen(80);
+var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
+
+app.listen(port);

ea3876f31bce6222abe7e0ab8f890869e69c9766
Lysandro Carioca <lysandroc@gmail.com>
2017-05-27 17:12:07 -0300

    static files to heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static('./public')); 
+
+app.get('/', function(request, response) {
+  response.render('./public/index.html');
+});
+
+app.listen(80);

6c1fee5c374992b74e7fbd24ef93b76664c4a91d
cjuniorr <celiopaivajr@gmail.com>
2017-05-27 14:47:48 -0300

    Upload de arquivos

---
+++ gulpTasks/server.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp')
+const watch = require('gulp-watch')
+const webserver = require('gulp-webserver')
+
+gulp.task('server', ['watch'], function(){
+    gulp.src('public').pipe(webserver({
+        livereload: true,
+        port: 3000,
+        open: true
+    }))
+})

7f161612b3f80cde5f3b2d09cc15a7b4b9f28993
Lysandro Carioca <lysandroc@gmail.com>
2017-05-27 16:42:32 -0300

    configurando deploy heroku

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ var path = require('path');
 
 app.use(express.static('./public')); 
 
+app.get('/', function(request, response) {
+  response.render('./public/index.html');
+});
+
 app.listen(8080);
